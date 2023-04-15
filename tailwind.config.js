914046e51fa17ce41af7e8e8556f2d6b6c09b8d4
Alissa Çenga <acenga20@epoka.edu.al>
2023-04-15 23:21:52 +0200

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "templates/**/*.{html,twig}",
+    "./assets/**/*.{js, ts, jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
